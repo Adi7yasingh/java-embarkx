dd27c654b2cbed1586c20fbd681807cee6bddaf0
Adi7yasingh <aditya9826864695@gmail.com>
2025-06-17 17:42:15 +0530

    init commit

---
+++ src/identifiers.java
@@ -0,0 +1,8 @@
+public class identifiers {
+    public static void main(String[] args) {
+        int First = 10;
+        int Second = 25; //First,Second are identifiers
+
+        System.out.println("identifiers" );
+    }
+}
