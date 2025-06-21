2390a62e5c36ed7ff347dff8c545d102056e0f8e
Adi7yasingh <aditya9826864695@gmail.com>
2025-06-21 18:28:34 +0530

    init commit

--- src/DataTypes4.java
+++ src/DataTypes4.java
@@ -1,7 +1,35 @@
 public class DataTypes4 {
     public static void main(String[] args) {
         /*
+        Primitive Data Types: Integers, floating point number, character, boolean.
 
+        INTEGERS
+        byte, short, int, long
+
+        DOUBLE
+        double, float
+
+        CHARACTER
+        char
+
+        BOOLEAN
+        boolean
          */
+
+        //8 bit or 1 byte
+        byte minByte = -128;
+        byte maxByte = 127;
+
+        //16 bit or 2 bytes
+        short minShort = -32_768;
+        short maxShort = 32_767;
+
+        //32 bit or 4 bytes
+        int minInteger = -2_147_483_648;
+        int maxInteger = 2_147_483_647;
+
+        //64 bit or 8 bytes
+        long minLong =-9223372036854775808L;
+        long maxLong = 9223372036854775807L;
     }
 }
