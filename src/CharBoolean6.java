8c2b39efa5acaa61164bd0cc29350dd4e17d915f
Adi7yasingh <aditya9826864695@gmail.com>
2025-06-22 12:39:27 +0530

    init commit

---
+++ src/CharBoolean6.java
@@ -0,0 +1,16 @@
+public class CharBoolean6 {
+    public static void main(String[] args) {
+
+        // memory size is 1bit
+        boolean myBoolean = true;
+        System.out.println("myBoolean- " + myBoolean);
+
+        //32bits or 8bytes
+        char myChar = 'A';
+        System.out.println("char is- " + myChar);
+
+        //char is also used to print unicodes(search on google)
+        char unicode = '\u00A9';
+        System.out.println("copyright- " + unicode);
+    }
+}
