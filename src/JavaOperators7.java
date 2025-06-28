0ed52fb3da0d4dfb11f26e0dc24004773bbf54ee
Adi7yasingh <aditya9826864695@gmail.com>
2025-06-28 13:42:29 +0530

    init commit

---
+++ src/JavaOperators7.java
@@ -0,0 +1,43 @@
+public class JavaOperators7 {
+    public static void main(String[] args) {
+        /*
+        = TYPES OF JAVA OPERATORS =
+
+        Arithmetic operators
+        + - * / % ++ --
+
+        Relational operators
+        == != > < >= <=
+
+        Logical operators
+        && || !
+
+        Assignment operators
+        = += -= *= /= %=
+
+        Conditional operators
+        ?:
+         */
+        int a = 10, b = 20, c = 15, d = 25;
+
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("b / a = " + (b / a));
+        System.out.println("c % a = " + (c % a));//prints remainder after dvision
+
+        a++; //equivalent to a = a + 1;
+        System.out.println("a++ = " + a); //increment
+
+        b--; //equivalent to a = a - 1;
+        System.out.println("a-- = " + b); //decrement
+
+
+
+
+
+
+
+
+    }
+}
