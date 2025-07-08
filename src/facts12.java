39f2d703131a068b670f9ecf5a8a054446a879fd
Adi7yasingh <aditya9826864695@gmail.com>
2025-07-08 18:27:38 +0530

    init commit

---
+++ src/facts12.java
@@ -0,0 +1,11 @@
+public class facts12 {
+    public static void main(String[] args) {
+        /*
+        Some facts about increment and decrement operators
+        1. we can use increment and decrement operators only with variables, but not with constants.
+        2. Nesting of increment and decrement operators is not allowed.
+        3. We can use increment and decrement operators with normal variables, but nt with final variables.
+        4. We cn apply increment and decrement operators for every primitive type but not with boolean(True;false)
+         */
+    }
+}
---
+++ src/main11.java
@@ -0,0 +1,14 @@
+public class main11 {
+    public static void main(String[] args) {
+        int x = 100;
+        System.out.println("value of ++x : " + ++x);
+        System.out.println("value of x-- : " + x--);
+        System.out.println("value of x : " + x);
+
+        /*
+        101
+        101
+        100
+         */
+    }
+}
