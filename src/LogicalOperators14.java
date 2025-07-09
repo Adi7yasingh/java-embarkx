3b8b2e2af2dfea38ca0ccadf43806143ad78c64f
Adi7yasingh <aditya9826864695@gmail.com>
2025-07-09 13:05:08 +0530

    init commit

---
+++ src/LogicalOperators14.java
@@ -0,0 +1,27 @@
+public class LogicalOperators14 {
+    public static void main(String[] args) {
+        /*
+        Logical operators
+        &&(AND) ||(OR) !(NOT)
+         */
+        int a = 10;
+        int b = 5;
+        boolean c = true;
+
+        // Logical AND (&&)
+        System.out.println((a > b) && (a < 20)); // true && true = true
+        System.out.println((a > b) && (a < 5));  // true && false = false
+
+        // Logical OR (||)
+        System.out.println((a < b) || (a > 5));  // false || true = true
+        System.out.println((a < b) || (a < 5));  // false || false = false
+
+        // Logical NOT (!)
+        System.out.println(!c); // !true = false
+        System.out.println(!(a == b)); // !(false) = true
+
+        //logical operators only take boolean values and also return boolean
+
+
+    }
+}
