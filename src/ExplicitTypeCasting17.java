69da90169ebacba60b1caa549324ad48b30b3208
Adi7yasingh <aditya9826864695@gmail.com>
2025-07-15 19:17:43 +0530

    init commit

--- src/ExplicitTypeCasting17.java
+++ src/ExplicitTypeCasting17.java
@@ -1,5 +1,19 @@
 public class ExplicitTypeCasting17 {
     public static void main(String[] args) {
+        int i = 100;
+        byte b = (byte)i; //byte is explicitly written
+        System.out.println("value of b= "+ b);
 
+        /*
+        points to remember
+        1. Explicit type casting is performed when value of larger data type is assigned to variable of smaller type.
+        2. Explicit type casting is performed by the programmer manually.
+        3. This is also known as narrowing or down casting.
+
+        possible conversions
+
+        double > float > long > int > short > byte
+        int > char
+         */
     }
 }
