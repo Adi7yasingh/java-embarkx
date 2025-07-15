abe45ddbb1c49d0760b6de923f4c87b785f79830
Adi7yasingh <aditya9826864695@gmail.com>
2025-07-15 18:39:14 +0530

    init commit

---
+++ src/ImplicitFirst16.java
@@ -0,0 +1,26 @@
+public class ImplicitFirst16 {
+    public static void main(String[] args) {
+    byte b = 100;
+    int i = b;
+        System.out.println("Value of i = " + i);
+
+    int i1 = 200;
+    double d = i1;
+        System.out.println("Value of d = " + d);
+
+    char c = 'a';
+    int c1 = c;
+        System.out.println("Value of c1 = "+ c1);
+/*
+    points to remember
+    1. Implicit type casting is performed when value of smaller data type is assigned to variable of larger data type.
+    2. Implicit type casting is performed by compiler.
+    3. Since the range of smaller data type is within the range of the larger data type, there is no loss in this type of conversion.
+    4. This is also known as widening or upcasting.
+
+    possible conversions
+    byte > short > int > long > float > double
+    char > int
+ */
+    }
+}
