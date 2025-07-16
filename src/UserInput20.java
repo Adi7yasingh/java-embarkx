5cbcdadf9d9b57b51581a65567ecb3f9271e7dff
Adi7yasingh <aditya9826864695@gmail.com>
2025-07-16 20:45:46 +0530

    init commit

--- src/UserInput20.java
+++ src/UserInput20.java
@@ -3,18 +3,19 @@ import java.util.Scanner;
 public class UserInput20 {
     public static void main(String[] args) {
         //accepting user input
-        /*Scanner myObj = new Scanner(System.in); // Create a Scanner object
+        /*
+        Scanner myObj = new Scanner(System.in); // Create a Scanner object
         System.out.println("Enter username = ");
 
         String userName = myObj.nextLine();  // Read user input
         System.out.println("Username is: " + userName);
         */
         System.out.println("Hello! What's your name? ");
-        Scanner sc = new Scanner(System.in);
-        String name = sc.nextLine();
+        Scanner sc = new Scanner(System.in); //creating scanner object
+        String name = sc.nextLine(); //read user input
 
         System.out.println("How old are you?");
-        int age = sc.nextInt();
+        int age = sc.nextInt(); //read user input
         System.out.println("You are "+ age + " years old ");
 
     }
