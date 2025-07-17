35c75b37dd1bb443fd004b769b18bf88cef9d0c9
Adi7yasingh <aditya9826864695@gmail.com>
2025-07-17 19:43:10 +0530

    init commit

--- src/IfElse22.java
+++ src/IfElse22.java
@@ -1,5 +1,12 @@
 public class IfElse22 {
     public static void main(String[] args) {
-
+    //Conditional statements
+        int age = 10;
+        if(age > 13){
+            System.out.println("Welcome to my website");
+        }
+        else{
+            System.out.println("Your age is less than 13");
+        }
     }
 }
