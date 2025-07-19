b49557977ff268fdc0d737274cb9d77528227e43
Adi7yasingh <aditya9826864695@gmail.com>
2025-07-19 22:47:24 +0530

    init commit

--- src/WhileLoop27.java
+++ src/WhileLoop27.java
@@ -1,5 +1,19 @@
 public class WhileLoop27 {
     public static void main(String[] args) {
+        /*
+        while(condition){
+          //statements
+        }
+         */
 
+        int counter = 0;
+
+        while(counter < 15){
+            System.out.println("I love Java");
+            counter++;
+        }
     }
 }
+
+
+
