1ceffe298ad2d6a170cd43423cef11440062e85c
Adi7yasingh <aditya9826864695@gmail.com>
2025-07-20 17:45:13 +0530

    init commit

---
+++ src/FactsWhileLoop28.java
@@ -0,0 +1,14 @@
+public class FactsWhileLoop28 {
+    public static void main(String[] args) {
+        /*
+        Facts about while loop
+        1.Condition in while loop should always be boolean, you will gwt compile time error if it's anything other than boolean.
+        example while(true){} is valid
+        but while(1){} is not valid because it is integer
+
+        2.Curly braces for while loops are option. but if they are not used then the while loop can have 1 statement, and it should not be a declaration.
+
+
+         */
+    }
+}
