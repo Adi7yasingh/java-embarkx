d4d80041a7d21d1398315d630f17f654834566d8
Adi7yasingh <aditya9826864695@gmail.com>
2025-07-22 19:05:06 +0530

    init commit

--- src/FactsWhileLoop28.java
+++ src/FactsWhileLoop28.java
@@ -3,12 +3,30 @@ public class FactsWhileLoop28 {
         /*
         Facts about while loop
         1.Condition in while loop should always be boolean, you will gwt compile time error if it's anything other than boolean.
-        example while(true){} is valid
+        example- while(true){} is valid
         but while(1){} is not valid because it is integer
 
         2.Curly braces for while loops are option. but if they are not used then the while loop can have 1 statement, and it should not be a declaration.
-
+        example- while(true){}, while(true);, while(true){int x = 100;}  these all are valid
+                 while(true) int x = 100; invalid
+        3. While loop is majorly used in scenarios where we dont know the number of iterations upfront.
 
          */
+
+        //example 1
+
+       /* while(true){
+        System.out.println("hello");
+        }
+        System.out.println("hi");
+        //invalid because it is an infinite loop (unreachable statement)
+         */
+
+        //example 2
+        while(false){
+            System.out.println("hello"); //loop body is unreachable, it will never execute this code
+        }
+        System.out.println("hi");
+
     }
 }
