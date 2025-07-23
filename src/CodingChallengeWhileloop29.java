d26f510012ee0fff30a68632d9b166172d089138
Adi7yasingh <aditya9826864695@gmail.com>
2025-07-23 18:16:34 +0530

    init commit

---
+++ src/CodingChallengeWhileloop29.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class CodingChallengeWhileloop29 {
+    public static void main(String[] args) {
+        /*
+        Write a java program to accept an integer from user and print factorial of the number using while loop.
+        factorial of 5(5!) = 5*4*3*2*1 = 120
+         */
+
+        System.out.println("Write a number to get its factorial");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int factorial = 1, i = 1;
+
+        while(a >= i){
+            factorial = factorial * i;
+            i++;
+        }
+
+        System.out.println("Factorial of " + a + " is " + factorial);
+
+    }
+}
