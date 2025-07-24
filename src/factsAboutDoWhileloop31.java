b1e70e7d29c69c3f6982485d27dd3bb9e56f5012
Adi7yasingh <aditya9826864695@gmail.com>
2025-07-24 15:09:29 +0530

    init commit

---
+++ src/factsAboutDoWhileloop31.java
@@ -0,0 +1,56 @@
+public class factsAboutDoWhileloop31 {
+    public static void main(String[] args) {
+        /*
+        Curly braces are optional and without curly braces we can only take one statement between do and while which should not be declarative.
+
+        do
+         System.out.println("Hello");
+        while(true);
+
+        //example 1
+
+        do{
+         System.out.println("Hello");
+        }while(true);
+        System.out.println("Hello"); //unreachable statement because it is never ending loop/infinite loop
+
+        //example 2
+
+        int a = 10, b = 20;
+        do{
+            System.out.println("Hello");
+        }while(a < b);
+        System.out.println("hi"); // even though it is an infinite loop , the compiler does not show any error
+                                  //because it is not substituting the value of a and b, because they are not hardcoded
+
+
+        //example 3
+
+        int a = 10, b = 20;
+        do{
+            System.out.println("Hello");
+        }while(a > b);
+        System.out.println("hi");
+
+
+        //example 4
+
+        final int a = 10, b = 20;
+        do{
+            System.out.println("Hello");
+        }while(a < b); //so now we have hardcoded value compiler know that it is always true, so it will be an infinite loop and never stop
+        System.out.println("hi"); //which will make it unreachable
+
+
+        //example 5
+
+        final int a = 10, b = 20;
+        do{
+            System.out.println("Hello");
+        }while(a > b);
+        System.out.println("hi");
+
+         */
+    }
+
+}
