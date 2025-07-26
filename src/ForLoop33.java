64048af4a960a157703124940b699094f13c3992
Adi7yasingh <aditya9826864695@gmail.com>
2025-07-26 13:13:41 +0530

    init commit

---
+++ src/ForLoop33.java
@@ -0,0 +1,23 @@
+public class ForLoop33 {
+    public static void main(String[] args) {
+        /*
+         for(initialization; condition; increment){
+              //statements
+
+         }
+         */
+
+        //print i love java 15 times
+
+        for (int i = 1; i <= 15; i++){
+            System.out.println(i + " I love java");
+        }
+
+        //we make it endless loop, it will be never ending loop
+        /*
+        for (;;){
+        System.out.println(i + " I love java");
+        }
+         */
+    }
+}
