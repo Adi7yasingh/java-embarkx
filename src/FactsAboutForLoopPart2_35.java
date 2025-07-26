80ff2546a8f3c8b4b71e6ddc2abf018ce0ef973d
Adi7yasingh <aditya9826864695@gmail.com>
2025-07-26 17:27:07 +0530

    init commit

---
+++ src/FactsAboutForLoopPart2_35.java
@@ -0,0 +1,59 @@
+public class FactsAboutForLoopPart2_35 {
+    public static void main(String[] args) {
+     /*
+      //example 1
+        for(int i =0; true; i++){
+          System.out.println("Hello");
+      }
+        System.out.println("Hi");
+
+
+      //example 2
+        for(int i =0; false; i++){
+            System.out.println("Hello");
+        }
+        System.out.println("Hi");
+
+
+      //example 3
+        for(int i =0; ; i++){
+            System.out.println("Hello");
+        }
+        System.out.println("Hi");
+
+
+      //example 4
+        int a = 10, b = 20;
+       for(int i = 0; a<b; i++){
+           System.out.println("Hello");
+       }
+        System.out.println("Hi");
+
+      //example 5
+        int a = 10, b = 20;
+        for(int i = 0; a>b; i++){
+            System.out.println("Hello");
+        }
+        System.out.println("Hi");
+
+
+       //example 6
+        final int a = 10, b = 20;
+        for(int i = 0; a<b; i++){
+            System.out.println("Hello");
+        }
+        System.out.println("Hi");
+
+
+        //example 7
+        final int a = 10, b = 20;
+        for(int i = 0; a>b; i++) {
+            System.out.println("Hello");
+        }
+        System.out.println("Hi");
+
+      */
+
+
+    }
+}
