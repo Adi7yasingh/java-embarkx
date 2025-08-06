7edda0c4c3d5c830531765e1fd47abdfc46c2ed6
Adi7yasingh <aditya9826864695@gmail.com>
2025-08-06 11:52:42 +0530

    init commit

--- src/SwitchStatement_38.java
+++ src/SwitchStatement_38.java
@@ -1,7 +1,33 @@
 public class SwitchStatement_38 {
     public static void main(String[] args) {
 
-//switch
+/*
+switch(variable/expression) {
+     case value:
+     //statement
+     break;
+     case value:
+     //statement
+     break;
+    }
+Rules :
+* variable in switch statement can only be int, byte, short, char, String, enum, integer, Byte, Short, Character
+* case label cannot have a variable, it has to be only constant
+*/
 
+        int month = 3;
+        switch(month){
+            case 1:
+                System.out.println("January");
+                break;
+            case 2:
+                System.out.println("February");
+                break;
+            case 3:
+                System.out.println("March");
+                break;
+
+        }
+        System.out.println("Out of Switch");
     }
 }
