57251d617efbb14a791f141422d493f5e8875e60
Adi7yasingh <aditya9826864695@gmail.com>
2025-08-13 13:11:27 +0530

    init commit

---
+++ src/FactsAboutSwitchStatement_39.java
@@ -0,0 +1,21 @@
+public class FactsAboutSwitchStatement_39 {
+    public static void main(String[] args) {
+
+      /*
+      1. Curly braces are mandatory when you are writing switch statements.
+         ex- switch(s){
+             }
+      2. Every case should be a compile time constant.
+         ex- final int x = 100;
+             switch(s){
+             case 1: {
+             }
+             case x:{
+             }
+             }
+
+       */
+
+
+    }
+}
